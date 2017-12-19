159dfdcd2d04cb287e91a660df8e08a820e9b516
root <root@vps483807.ovh.net>
2017-12-19 16:11:47 +0100

    changing the voting options

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "futbol"
+optionB = "soccer"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
